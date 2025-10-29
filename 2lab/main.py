ca089ae590e500d4d9e167dbb44b985438fa19db
253453 <milana.khafizova1010@mail.ru>
2025-10-30 00:58:51 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,26 @@
+def frequency_mapping(str1, str2):
+    groups1 = {}
+    groups2 = {}
+    for char in set(str1):
+        count = str1.count(char)
+        groups1[count] = groups1.get(count, []) + [char]
+    
+    for char in set(str2):
+        count = str2.count(char)
+        groups2[count] = groups2.get(count, []) + [char]
+    
+    result = []
+    for count in sorted(groups1.keys()):
+        if count not in groups2 or len(groups1[count]) != len(groups2[count]):
+            return "Невозможно установить однозначное соответствие"
+        
+        
+        for a, b in zip(sorted(groups1[count]), sorted(groups2[count])):
+            result.append(f"{a}={b}")
+    
+    return " ".join(result)
+
+print(frequency_mapping("abcbaa", "jkekjj"))  
+print(frequency_mapping("aab", "dcc"))   
 
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
